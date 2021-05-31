96b27676c2780f5ec6c1e0618d6edcc795932768
qiujiangkun <qiujiangkun@foxmail.com>
2021-05-31 22:16:57 +0800

    missing BlockOnStatic

--- src/core/block_on.rs
+++ src/core/block_on.rs
@@ -5,3 +5,9 @@ pub trait BlockOn {
     /// The entry point of the executor
     fn block_on<F: Future>(&self, future: F) -> F::Output;
 }
+
+/// The entry point of the executor
+pub trait BlockOnStatic {
+    /// The entry point of the executor
+    fn block_on<F: Future>(future: F) -> F::Output;
+}
